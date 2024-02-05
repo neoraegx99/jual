a126900fba467504d0b6df5b69b041f5e592c73c
neoraegx99 <79630561+neoraegx99@users.noreply.github.com>
2024-02-06 00:19:22 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -76,7 +76,7 @@ CEKEXPIRED () {
     exit 0
 fi
 }
-IZIN=$(curl -sS https://raw.githubusercontent.com/awanklod/jual/main/izinnya | awk '{print $4}' | grep $MYIP)
+IZIN=$(curl -sS https://raw.githubusercontent.com/neoraegx99/jual/main/izinnya | awk '{print $4}' | grep $MYIP)
 if [ $MYIP = $IZIN ]; then
 echo -e "\e[32mPermission Accepted...\e[0m"
 CEKEXPIRED
